b345151cbb9d2cef95273082902da59cceb13323
NIKHILESH PATIL <nikhilspatilglb@gmail.com>
2025-07-02 00:18:09 +0200

    changes

--- src/main/java/InvoiceApproval.java
+++ src/main/java/InvoiceApproval.java
@@ -1,6 +1,6 @@
 public class InvoiceApproval {
     public static String approveInvoice(double amount) {
-        System.out.println("Approving invoice for amount: " + amount); // 🔥 New line
+        System.out.println("Approving invoice for amount123: " + amount); // 🔥 New line
         if (amount < 0)
             return "Reject: Invalid amount";
         if (amount > 10000)
