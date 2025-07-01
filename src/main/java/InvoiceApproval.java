dab50388e40a32d254506d99deaad3446108ae88
NIKHILESH PATIL <nikhilspatilglb@gmail.com>
2025-07-01 17:43:15 +0200

    No  changes

--- src/main/java/InvoiceApproval.java
+++ src/main/java/InvoiceApproval.java
@@ -1,11 +1,10 @@
 public class InvoiceApproval {
     public static String approveInvoice(double amount) {
-        if (amount < 0) return "Reject: Invalid amount";
-        if (amount > 10000) return "Requires Manager Approval";
+        if (amount < 0)
+            return "Reject: Invalid amount";
+        if (amount > 10000)
+            return "Requires Manager Approval";
         return "Auto-Approved";
     }
+
 }
-
-// Add a simple print statement inside a method or test, for example:
-
-System.out.println("Test change to verify Jenkins build!");
