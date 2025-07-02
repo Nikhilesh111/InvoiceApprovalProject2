e20e8290a206153da46b7f7ee69c837f89c25de3
NIKHILESH PATIL <nikhilspatilglb@gmail.com>
2025-07-02 19:51:59 +0200

    modified

--- src/main/java/InvoiceApproval.java
+++ src/main/java/InvoiceApproval.java
@@ -1,31 +1,3 @@
-// public class InvoiceApproval {
-//     public static String approveInvoice(double amount) {
-//         System.out.println("Approving invoice for amount123: " + amount); // 🔥 New line
-//         if (amount < 0)
-//             return "Reject: Invalid amount";
-//         if (amount > 10000)
-//             return "Requires Manager Approval";
-//         return "Auto-Approved";
-//     }
-
-// }
-
-// import java.util.logging.Logger;
-
-// public class InvoiceApproval {
-//     private static final Logger logger = Logger.getLogger(InvoiceApproval.class.getName());
-
-//     public static String approveInvoice(double amount) {
-//         logger.info("Approving invoice for amount: " + amount);
-
-//         if (amount <= 0)
-//             return "Reject: Invalid amount";
-//         if (amount > 10000)
-//             return "Requires Manager Approval";
-//         return "Auto-Approved";
-//     }
-// }
-
 import java.util.logging.Logger;
 
 public class InvoiceApproval {
@@ -35,7 +7,7 @@ public class InvoiceApproval {
         logger.info("Approving invoice for amount: " + amount);
 
         if (amount < 0) {
-            logger.warning("Rejecting invoice due to invalid amou");
+            logger.warning("Rejecting invoice due to invalid amount");
             return "Reject: Invalid amount";
         }
         if (amount > 10000) {
